462e60742f2a676622f82a9511bed302f951c0e7
Kishan Patel <kbpatel309@gmail.com>
2025-01-30 23:55:48 -0600

    fixed indentation

--- components/navbar/profile/index.tsx
+++ components/navbar/profile/index.tsx
@@ -1,28 +1,30 @@
 import React from 'react'
-import Logo from '../logo'
 import Link from 'next/link'
-import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export default function Profile() {
-    const isLoggedIn = true
+    const isLoggedIn = true;
     return (
         <>
-        {isLoggedIn ? (
-            <div className='flex items-center gap-5'>
-                <div className='flex items-center gap-3'>
-                    <Link href={`/user/$user.id`} className='text-neutral-400 font-semibold hover:underline'>
-                        First Last
-                    </Link>
-                    <Avatar className="w-8 h-8">
-                        <AvatarImage src="https://github.com/kbpatel309"></AvatarImage>
-                    </Avatar>
+            {isLoggedIn ? (
+                <div className='flex items-center gap-5'>
+                    <div className='flex items-center gap-3'>
+                        <Link href={`/user/$user.id`} className='text-neutral-400 font-semibold hover:underline'>
+                            First Last
+                        </Link>
+                        <Avatar className="w-8 h-8">
+                            <AvatarImage src="https://github.com/kbpatel309.png" />
+                            <AvatarFallback>
+                                KP
+                            </AvatarFallback>
+                        </Avatar>
+                    </div>
                 </div>
-            </div>
-        ) : (
-            <div>
-                
-            </div>
-        )}
+            ) : (
+                <div>
+                    
+                </div>
+            )}
         </>
-    )
+    );
 }
