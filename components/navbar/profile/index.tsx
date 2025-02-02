2f9d7d3a9a16844c7b144e9cf896208dac68e1e4
Kishan Patel <kbpatel309@gmail.com>
2025-02-01 23:58:25 -0600

    included GoogleLogin component

--- components/navbar/profile/index.tsx
+++ components/navbar/profile/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { GoogleLogin } from "@react-oauth/google"
 
 export default function Profile() {
     const isLoggedIn = true;
+    const onSuccess = (res: { credentials: string }) => {
+
+    }
     return (
         <>
             {isLoggedIn ? (
@@ -21,7 +25,7 @@ export default function Profile() {
                     </div>
                 </div>
             ) : (
-                <div></div>
+                <GoogleLogin onSuccess={res => onSuccess(res)} />
             )}
         </>
     );
