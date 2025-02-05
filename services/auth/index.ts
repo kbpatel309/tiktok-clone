6b47e365c159022e2e3760cbf586a989f70345a5
Kishan Patel <kbpatel309@gmail.com>
2025-02-04 23:59:10 -0600

    sanity

--- services/auth/index.ts
+++ services/auth/index.ts
@@ -1,7 +1,19 @@
 "use server"
 import jwt from "jsonwebtoken"
 
+export type Decoded = {
+    email: string;
+    name: string;
+    picture: string;
+    sub: string;
+}
+
 export async function auth(credential: string){
     const decoded = jwt.decode(credential)
-    console.log(decoded)
+    if(!decoded) return
+    createUser(decoded)
+    // create user
+        // create user only if it doesn't exist
+        // if user exists, don't create it
+    // create token
 }
