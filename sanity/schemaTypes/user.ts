db90b912f30cd6546f0d68c5b38f0ebf8b197a8d
Kishan Patel <kbpatel309@gmail.com>
2025-02-08 23:57:44 -0600

    picture url

--- sanity/schemaTypes/user.ts
+++ sanity/schemaTypes/user.ts
@@ -1,6 +1,24 @@
-import { defineType } from "sanity";
+import { defineField, defineType } from "sanity";
 
 export const userType = defineType({
     title: "User",
     name: "user",
+    type: "document",
+    fields: [
+        defineField({
+            title: "Email",
+            name: "email",
+            type: "string"
+        }),
+        defineField({
+            title: "Name",
+            name: "name",
+            type: "string"
+        }),
+        defineField({
+            title: "Picture URL",
+            name: "picture_url",
+            type: "string"
+        }),
+    ]
 })
