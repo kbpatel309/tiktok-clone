eb435e7c4aacaec24ed61dc8e03ec32c4d3a27bb
Kishan Patel <kbpatel309@gmail.com>
2025-02-10 23:57:47 -0600

    updated few lines of code

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { userType } from './user'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [userType],
 }
--- services/user/index.ts
+++ services/user/index.ts
@@ -1,4 +1,5 @@
 import { sanity } from "@/sanity/lib/client";
+import { Decoded } from "../auth";
 
 export async function createUser(decodedUser: Decoded) {
     sanity.createIfNotExists()
