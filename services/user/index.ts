878c9ba29e1ad51294ae0ca224f229d79e99e7c7
Kishan Patel <kbpatel309@gmail.com>
2025-02-13 23:57:37 -0600

    added code to user/index.ts

--- services/user/index.ts
+++ services/user/index.ts
@@ -2,5 +2,9 @@ import { sanity } from "@/sanity/lib/client";
 import { Decoded } from "../auth";
 
 export async function createUser(decodedUser: Decoded) {
+    const doc = {
+        _type: "user",
+        _id: decodedUser.sub,
+    }
     sanity.createIfNotExists()
 }
