a15a1bd17b16a480928a4b52f745c67b0fcd30df
Kishan Patel <kbpatel309@gmail.com>
2025-03-12 23:58:25 -0500

    created token

--- services/auth/index.ts
+++ services/auth/index.ts
@@ -1,6 +1,9 @@
 "use server"
 import jwt from "jsonwebtoken"
+import { createUser } from "../user";
+import { cookies } from "next/headers";
 
+const privateKey = process.env.NEXT_PUBLIC_JWT_SECRET || ""
 export type Decoded = {
     email: string;
     name: string;
@@ -13,5 +16,10 @@ export async function auth(credential: string){
     if(!decoded) return
     const user = await createUser(decoded as Decoded)
     if(!user) return
-    // create tokens
+
+    // create a token
+    const token = jwt.sign({ _id: user._id }, privateKey, { expiresIn: "30d" })
+    const maxAge = 60 * 60 * 60 * 24 * 30 // 30 days
+    cookies().set({ name: "auth_token", value: token, maxAge })
+    return user
 }
